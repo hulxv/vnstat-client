0038ca7d457749d75c2eab3970ae463d24b7488c
“Hulxv” <m7mdx37@gmail.com>
2021-12-08 23:09:27 +0200

    Start work on 'vnStat' Settings

--- renderer/components/Settings/vnStat.js
+++ renderer/components/Settings/vnStat.js
@@ -1,5 +1,53 @@
+import {
+	Box,
+	Flex,
+	Heading,
+	Stack,
+	HStack,
+	Button,
+	Tooltip,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+import {
+	BsPlayFill,
+	BsPauseFill,
+	BsArrowCounterclockwise,
+} from "react-icons/bs";
+
 function vnStat() {
-	return <div>vnStat (soon...)</div>;
+	const [daemonStatus, setDaemonStatus] = useState(false);
+	return (
+		<Stack w='full'>
+			<Stack w='full'>
+				<Heading size='md' alignSelf='center'>
+					Daemon
+				</Heading>
+				<Flex w='full' justify='space-between'>
+					<Box>Status: {daemonStatus ? "Acitve " : "Idle"}</Box>
+					<HStack>
+						<Button
+							leftIcon={<BsArrowCounterclockwise size='1.4em' />}
+							variant='ghost'>
+							Restart
+						</Button>
+						<Button
+							colorScheme={`${daemonStatus ? "red" : "green"}`}
+							leftIcon={
+								daemonStatus ? (
+									<BsPauseFill size='1.4em' />
+								) : (
+									<BsPlayFill size='1.4em' />
+								)
+							}
+							onClick={() => setDaemonStatus(!daemonStatus)}>
+							{daemonStatus ? "Stop" : "Start"}
+						</Button>
+					</HStack>
+				</Flex>
+			</Stack>
+		</Stack>
+	);
 }
 
 export default vnStat;
