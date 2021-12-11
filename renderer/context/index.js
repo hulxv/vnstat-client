9da286287c5e148036c51ac9f1d218b6c52172c4
“Hulxv” <m7mdx37@gmail.com>
2021-12-11 23:50:42 +0200

    create shortcuts context

--- renderer/context/index.js
+++ renderer/context/index.js
@@ -2,15 +2,18 @@ import UsageProvider from "./dataUsage";
 import ReceviedMessagesProvider from "./recevied-messages";
 import Configration from "./configration";
 import Logs from "./logs";
+import Shortcuts from "./shortcuts";
 
 function Contexts({ children }) {
 	return (
 		<Configration>
-			<ReceviedMessagesProvider>
-				<UsageProvider>
-					<Logs>{children}</Logs>
-				</UsageProvider>
-			</ReceviedMessagesProvider>
+			<Shortcuts>
+				<ReceviedMessagesProvider>
+					<UsageProvider>
+						<Logs>{children}</Logs>
+					</UsageProvider>
+				</ReceviedMessagesProvider>
+			</Shortcuts>
 		</Configration>
 	);
 }
--- renderer/context/shortcuts.js
+++ renderer/context/shortcuts.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext, createContext } from "react";
 
-const ShortcutsProvider = createContext({});
+const ShortcutsProvider = createContext({ children });
 
 function Shortcuts() {
 	const [shortcuts, setShortcuts] = useState([{ key: "", function: () => {} }]);
@@ -8,7 +8,9 @@ function Shortcuts() {
 		shortcuts,
 	};
 	return (
-		<ShortcutsProvider.Provider value={value}>Enter</ShortcutsProvider.Provider>
+		<ShortcutsProvider.Provider value={value}>
+			{children}
+		</ShortcutsProvider.Provider>
 	);
 }
 
