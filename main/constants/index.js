810c1f6f2f0baa87c047a201448f0cbefd678817
hulxv <hulxxv@gmail.com>
2026-05-30 21:15:47 +0300

    remove electron

--- main/constants/index.js
+++
@@ -1,1 +0,0 @@
-export const ICON_NAME = "vnclient-icon.png";
--- renderer/components/Server/index.js
+++
@@ -1,4 +0,0 @@
-import ConnectModal from "./ConnectModal";
-import DisconnectModal from "./DisconnectAlert";
-
-export { ConnectModal, DisconnectModal };
